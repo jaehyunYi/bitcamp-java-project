4d25c99acd7fc5c1ed26eb72224394dfe7c37c1a
jaehyunYi <68311209+jaehyunYi@users.noreply.github.com>
2020-07-31 19:27:44 +0900

    Add files via upload

---
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/App2.java
@@ -0,0 +1,71 @@
+package com.eomcs.pms;
+
+import java.sql.Date;
+import java.util.Scanner;
+
+public class App2 {
+
+  public static void main(String[] args) {
+    System.out.println("[프로젝트]");
+
+    Scanner sc = new Scanner(System.in);
+
+    // 최대 100개의 프로젝트 정보를 저장할 메모리 준비
+    // => 배열의 크기를 미리 변수에 저장하여 사용한다.
+    // => 나중에 배열의 크기를 바꾸기 쉽다.
+    int length = 100;
+
+    int[] no = new int[length];
+    String[] title = new String[length];
+    String[] content = new String[length];
+    Date[] startDate = new Date[length];
+    Date[] endDate = new Date[length];
+    String[] owner = new String[length];
+    String[] members = new String[length];
+
+    int count = 0;
+    for (int i = 0; i < 100; i++) {
+      System.out.print("번호? ");
+      no[i] = Integer.valueOf(sc.nextLine());
+
+      System.out.print("프로젝트명? ");
+      title[i] = sc.nextLine();
+
+      System.out.print("내용? ");
+      content[i] = sc.nextLine();
+
+      System.out.print("시작일? ");
+      startDate[i] = Date.valueOf(sc.nextLine());
+
+      System.out.print("종료일? ");
+      endDate[i] = Date.valueOf(sc.nextLine());
+
+      System.out.print("만든이? ");
+      owner[i] = sc.nextLine();
+
+      System.out.print("팀원? ");
+      members[i] = sc.nextLine();
+
+      count++;
+      System.out.println(); // 빈 줄 출력
+
+      System.out.print("계속 입력하시겠습니까?(Y/N) ");
+      String str = sc.nextLine();
+      if (!str.equalsIgnoreCase("y")) {
+        break;
+      }
+      System.out.println(); // 빈 줄 출력
+    }
+
+    sc.close();
+
+    System.out.println("--------------------------------");
+
+
+    for (int i = 0; i < count; i++) {
+      // 번호, 프로젝트명, 시작일, 종료일, 만든이
+      System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
+          no[i], title[i], startDate[i], endDate[i], owner[i]);
+    }
+  }
+}
